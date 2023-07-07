9bee5cb2f3c1b0a4a6b7627f10b06e6e93831c13
Oskar Dabrowski <oskar.dabrowski12890@wp.pl>
2023-07-07 22:45:09 +0200

    POST Decorator

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
+import { Task } from './task.model';
 
 @Controller('tasks')
 export class TasksController {
     constructor(private TasksService: TasksService) {}
 
     @Get()
-    getAllTasks() {
+    getAllTasks(): Task[] {
         return this.TasksService.getAllTasks();
+    }
+
+    @Post()
+    createTask(
+        @Body('title') title: string,
+        @Body('description') description: string,
+    ): Task {
+        return this.TasksService.createTask(title, description);
     }
 }
