525484f1925b9e49b63fe28ccfef044de6bf42d0
Oskar Dabrowski <oskar.dabrowski12890@wp.pl>
2023-07-08 23:14:17 +0200

    implement geting tasks by id - multiple get decorators

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './task.model';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Controller('tasks')
 export class TasksController {
@@ -11,11 +12,15 @@ export class TasksController {
         return this.TasksService.getAllTasks();
     }
 
+    @Get('/:id')
+    getTaskById(@Param('id') id:string):Task {
+        return this.TasksService.getTaskById(id);
+    }
+
     @Post()
     createTask(
-        @Body('title') title: string,
-        @Body('description') description: string,
+        @Body() CreateTaskDto: CreateTaskDto
     ): Task {
-        return this.TasksService.createTask(title, description);
+        return this.TasksService.createTask(CreateTaskDto);
     }
 }
