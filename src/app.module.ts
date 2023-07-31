66b9ded4875e23d202776175697f11aa555edbc7
Oskar Dabrowski <oskar.dabrowski12890@wp.pl>
2023-07-31 22:45:04 +0200

    Problem with TypeORM .env module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,20 +2,41 @@ import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    TasksModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: "#######",
-      database: 'task-management',
-      autoLoadEntities: true,
-      synchronize: true
+    ConfigModule.forRoot({
+      envFilePath: [`.env.stage.${process.env.STAGE}`],
+      isGlobal: true,
     }),
+    TasksModule,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (ConfigService: ConfigService) => {
+        console.log(process.env.STAGE);
+        console.log(process.env);
+        console.log(process);
+        console.log(ConfigService.get<string>('DB_HOST'));
+        console.log(ConfigService.get<number>('DB_PORT'));
+        console.log(ConfigService.get<string>('DB_USERNAME'));
+        console.log(ConfigService.get<string>('DB_PASSWORD'));
+        console.log(ConfigService.get<string>('DB_DATABASE'));
+
+        return {
+          type: 'postgres',
+          autoLoadEntities: true,
+          synchronize: true,
+          host: ConfigService.get('DB_HOST'),
+          port: ConfigService.get('DB_PORT'),
+          username: ConfigService.get('DB_USERNAME'),
+          password: ConfigService.get('DB_PASSWORD'),
+          database: ConfigService.get('DB_DATABASE'),
+        }
+      }
+    }),
+
     AuthModule
   ],
 })
